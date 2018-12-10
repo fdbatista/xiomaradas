cf367f0fdb34edca9b0259d28e5aa14130db3dcd
fdbatista <fdbatista@gmail.com>
2018-12-10 16:37:48 -0500

    Tagging support added

--- views/events/_form.php
+++ views/events/_form.php
@@ -1,11 +1,14 @@
 <?php
 
+use app\models\Event;
+use kartik\select2\Select2;
 use yii\helpers\Html;
+use yii\web\View;
 use yii\widgets\ActiveForm;
 
-/* @var $this yii\web\View */
-/* @var $model app\models\Event */
-/* @var $form yii\widgets\ActiveForm */
+/* @var $this View */
+/* @var $model Event */
+/* @var $form ActiveForm */
 ?>
 
 <div class="event-form">
@@ -16,6 +19,28 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
 
+    <?php
+    echo '<label class="control-label">Tag Multiple</label>';
+    echo Select2::widget([
+        'name' => 'color_2',
+        'value' => ['red', 'green'], // initial value
+        'data' => ['red', 'green', 'blue', 'yellow'],
+        'maintainOrder' => true,
+        'toggleAllSettings' => [
+            'selectLabel' => '<i class="fas fa-ok-circle"></i> Tag All',
+            'unselectLabel' => '<i class="fas fa-remove-circle"></i> Untag All',
+            'selectOptions' => ['class' => 'text-success'],
+            'unselectOptions' => ['class' => 'text-danger'],
+        ],
+        'options' => ['placeholder' => 'Select a color ...', 'multiple' => true],
+        'pluginOptions' => [
+            'tags' => true,
+            'maximumInputLength' => 10
+        ],
+    ]);
+    ?>
+
+    <br />
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
     </div>
